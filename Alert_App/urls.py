edf4b36fed2ea91d58d6a1dafe605a73b23954eb
ChrisBlanks <christopherablanks@gmail.com>
2019-01-23 14:18:39 -0500

    Added in ability to switch between pages

--- Alert_App/urls.py
+++ Alert_App/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from django.views.generic.base import TemplateView, RedirectView
 
 from . import views
 
-app_name = "Alert_App" 
+app_name = "AlertApp" 
 urlpatterns = [
     path('',views.index,name="index"),
+	path('report/',views.report,name="report"),
 ]
+
+#For regular expression:
+# r = raw string ;  ^ = matches the start of a string
+# (\d+) = matches all numbers for one or more expressions 
+# $ = matches the end
--- Alert_App/views.py
+++ Alert_App/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("You logged in.")
+	device_id = None
+	user_type = None
+	if request.method == "POST":
+		device_id = request.POST.get('id_number',None)
+		user_type = request.POST.get('user_type',None)
+		print (user_type)
+		print(user_type)
+		print(user_type)
+		#print(request.user.is_authenticated)
+		#print(request.user.is_supervisor_user)
+    
+	if device_id == None or device_id == "":
+		msg = "Logged in, but not using a registered device."
+	else:
+		msg = "Logged in with device: " + str(device_id)
+		
+	context = {'message': msg,'user_type':user_type}
+	return render(request,'Alert_App/index.html', context)
+	
+
+def report(request):
+	"""Sets up view for reporting emergencies."""
+	user_type = None 
+	if request.method == "POST":
+		user_type = request.POST.get('user_type',None)
+	context = {'user_type':user_type}
+	return render(request,'Alert_App/report.html',context)
+	
+
+

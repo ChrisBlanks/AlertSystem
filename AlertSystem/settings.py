43d15df9e5e1455d3fecf3825bb8970044e829fc
ChrisBlanks <cablanks@hotmail>
2019-01-23 14:18:39 -0500

    Added in ability to switch between pages

--- AlertSystem/settings.py
+++ AlertSystem/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'AlertSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'),os.path.join(BASE_DIR,'Alert_App/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Alert_App/models.py
+++ Alert_App/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
-# Create your models here.
+class Users(models.Model):
+	user = models.OneToOneField(User,on_delete=models.CASCADE)
+	is_supervisor_user = models.BooleanField(default=False)
+	
+	def get_supervisor_status(self):
+		"""Returns status of whether a user is a supervisor or not."""
+		return self.is_supervisor_user
+	
+	def get_is_authenticated(self):
+		"""Returns authentication value."""
+		return self.is_authenticated

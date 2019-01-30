33769f58bb53e2fe4659cc34889f961119cadf43
ChrisBlanks <christopherablanks@gmail.com>
2019-01-30 17:13:16 -0500

    Removed unnecessary file

--- Alert_App/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-from Alert_App.models import Marker, DeviceId
-
-class UsersForm(forms.ModelForm):
-    class Meta:
-        model = Users
-        fields = ('phone','age','gender','address','bio')
